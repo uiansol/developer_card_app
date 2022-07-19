1b59c71b7acda61c95e63270610746ef64152d7b
Uian Sol <usoldeveloper@gmail.com>
2022-07-19 17:11:35 -0300

    Add statless widget to enable hotreload

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
