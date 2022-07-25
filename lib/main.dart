ab2ca665cd7d803286ff9225ea239e4ec8fbe3f9
Uian Sol <usoldeveloper@gmail.com>
2022-07-25 15:08:55 -0300

    add image and start column structure

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/uian1.jpg'),
+              ),
+              Text(
+                'Uian Sol',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
